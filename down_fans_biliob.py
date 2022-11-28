a4f4e5165d3c055dcf08f71ca1626d51ec9e0cbd
XXXShaunPan <80847088+XXXShaunPan@users.noreply.github.com>
2022-11-28 17:29:29 +0800

    Update down_fans_biliob.py

--- down_fans_biliob.py
+++ down_fans_biliob.py
@@ -17,19 +17,38 @@ header={
 }
 
 df.loc[time]=[0]*len(df.columns)
-def spider():
+def spider_json():
 	res=rq.get('https://zeroroku.com/_next/data/sW-UJcYRbybO0jl2Ul68z/bilibili/rank/rate1/asc.json?field=rate1&order=asc',headers=header,timeout=(60,60)).json()
 	return res['pageProps']['data']
 	
+def spider():
+	res=rq.get('https://zeroroku.com/bilibili/rank/rate1/asc',headers=header,timeout=(60,60)).text
+	return res
 
-def proc():
-	data=spider()
+def proc_json():
+	data=spider_json()
 	for i in data[:21]:
 		name=i['name']
 		uid=i['mid']
 		cRate=abs(i['stats']['rate1'])
-# 		name,uid=doc(f'.gap-3:eq({i}) .flex-1 div').text().split(' UID: ')
-# 		cRate=doc(f'.gap-3:eq({i}) .flex-shrink div').text()[1:].replace(',','')
+		if name not in df.columns:
+			df[name]=[0]*len(df.index)
+			df[name]['mid']=uid
+		df[name][time]=cRate
+		# if not os.path.exists(f'pic_down_biliob/{i["name"]}.jpg'):
+		# 	with open(f'pic_down_biliob/{i["name"]}.jpg','wb') as f:
+		# 		f.write(rq.get(i['face']).content)
+		print(name,cRate)
+	
+	
+def proc():
+	data=spider()
+	for i in range(21):
+# 		name=i['name']
+# 		uid=i['mid']
+# 		cRate=abs(i['stats']['rate1'])
+		name,uid=doc(f'.gap-3:eq({i}) .flex-1 div').text().split(' UID: ')
+		cRate=doc(f'.gap-3:eq({i}) .flex-shrink div').text()[1:].replace(',','')
 		if name not in df.columns:
 			df[name]=[0]*len(df.index)
 			df[name]['mid']=uid
@@ -40,7 +59,9 @@ def proc():
 		print(name,cRate)
 
 if __name__ == '__main__':
-	proc()
-
+	try:
+		proc()
+	except:
+		proc_json()
 	df=df.drop_duplicates()
 	df.to_csv('bili_fans/down_fans.csv',header=True,index=True)
