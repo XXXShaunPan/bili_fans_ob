0f83dde214c2fcbdaf6268aff8bbda916725be4f
XXXShaunPan <80847088+XXXShaunPan@users.noreply.github.com>
2023-05-24 15:52:29 +0800

    Update up_fans_biliob.py

--- up_fans_biliob.py
+++ up_fans_biliob.py
@@ -19,8 +19,8 @@ header={
 }
 
 def spider_json():
-	res=rq.get('https://zeroroku.com/_next/data/sW-UJcYRbybO0jl2Ul68z/bilibili/rank/rate1/desc.json?field=rate1&order=desc',headers=header,timeout=(60,60)).json()
-	return res['pageProps']['data']
+	res=rq.get('https://api.zeroroku.com/bilibili/rank?f=rate1&o=0',headers=header,timeout=(60,60)).json()
+	return res
 	
 def spider():
 	res=rq.get('https://zeroroku.com/bilibili/rank/rate1/desc',headers=header,timeout=(60,60)).text
@@ -42,30 +42,28 @@ def proc_json():
 		print(name,cRate)
 	
 	
-def proc():
-	data=spider()
-	for i in range(1,22):
-# 		name=i['name']
-# 		uid=i['mid']
-# 		cRate=abs(i['stats']['rate1'])
-		doc = pq(data)
-		uid=doc(f'.gap-3:eq({i}) .flex-1 div').text().replace('UID:','')
-		name = doc(f'.gap-3:eq({i}) .flex-1 a').text()
-		cRate=doc(f'.gap-3:eq({i}) .flex-shrink div').text()[1:].replace(',','')
-		if name not in df.columns:
-			df[name]=[0]*len(df.index)
-			df[name]['mid']=uid
-		df[name][time]=cRate
-		# if not os.path.exists(f'pic_down_biliob/{i["name"]}.jpg'):
-		# 	with open(f'pic_down_biliob/{i["name"]}.jpg','wb') as f:
-		# 		f.write(rq.get(i['face']).content)
-		print(name,cRate)
+# def proc():
+# 	data=spider()
+# 	for i in range(1,22):
+# # 		name=i['name']
+# # 		uid=i['mid']
+# # 		cRate=abs(i['stats']['rate1'])
+# 		doc = pq(data)
+# 		uid=doc(f'.gap-3:eq({i}) .flex-1 div').text().replace('UID:','')
+# 		name = doc(f'.gap-3:eq({i}) .flex-1 a').text()
+# 		cRate=doc(f'.gap-3:eq({i}) .flex-shrink div').text()[1:].replace(',','')
+# 		if name not in df.columns:
+# 			df[name]=[0]*len(df.index)
+# 			df[name]['mid']=uid
+# 		df[name][time]=cRate
+# 		# if not os.path.exists(f'pic_down_biliob/{i["name"]}.jpg'):
+# 		# 	with open(f'pic_down_biliob/{i["name"]}.jpg','wb') as f:
+# 		# 		f.write(rq.get(i['face']).content)
+# 		print(name,cRate)
 
 if __name__ == '__main__':
-	try:
-		proc()
-	except:
-		proc_json()
+
+	proc_json()
 	df=df.drop_duplicates()
 	df.to_csv('bili_fans/up_fans.csv',header=True,index=True)
 
