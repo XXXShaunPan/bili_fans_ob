4afdb4556c31910d50f34c1e79257fa5d777c837
XXXShaunPan <80847088+XXXShaunPan@users.noreply.github.com>
2024-02-06 17:58:39 +0800

    Update down_fans_biliob.py

--- down_fans_biliob.py
+++ down_fans_biliob.py
@@ -1,13 +1,15 @@
 import requests as rq
 import pandas as pd
 from datetime import datetime
-from pyquery import PyQuery as pq
+# from pyquery import PyQuery as pq
+from sqlalchemy import create_engine
 import os
 from pytz import timezone
 
 time=datetime.now(timezone('Asia/Shanghai')).strftime('%Y-%m-%d::%H')
+con = create_engine('mysql+pymysql://oujiarong:Ul7BRXbzETgjdZd2cbhWbn@39.108.78.157:3306/Win_Dataprocuremen?charset=utf8')
 #df=pd.DataFrame(index=[time])
-df=pd.read_csv('bili_fans/down_fans.csv',index_col=[0])
+# df=pd.read_csv('bili_fans/down_fans.csv',index_col=[0])
 header={
 	'Connection':'keep-alive', 
 	'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36', 
@@ -16,7 +18,7 @@ header={
 	'Accept-Language':'zh-CN,zh;q=0.9'
 }
 
-df.loc[time]=[0]*len(df.columns)
+# df.loc[time]=[0]*len(df.columns)
 def spider_json():
 	res=rq.get('https://api.zeroroku.com/bilibili/rank?f=rate1&o=1&s=30',headers=header,timeout=(60,60)).json()
 	return res
@@ -60,10 +62,30 @@ def proc_json():
 # 		# 		f.write(rq.get(i['face']).content)
 # 		print(name,cRate)
 
+data = {
+    'item_data': "Hi, I'm Shoto! I'm a rogue, demon slayer, and guild leader",
+    'item_time': time,
+    'is_down': 1
+}
+def main():
+	sql = "INSERT INTO ob_fan_data (item_data, item_time, is_down) VALUES (%(item_data)s, %(item_time)s, %(is_down)s)"
+	res = rq.get('https://api.zeroroku.com/bilibili/rank?f=rate1&o=1&s=30',headers=headers).text
+	data.update({'item_data': res})
+	con.execute(sql, data)
+	sql = "INSERT INTO ob_fan_data (item_data, item_time, is_down) VALUES (%(item_data)s, %(item_time)s, %(is_down)s)"
+	res = rq.get('https://api.zeroroku.com/bilibili/rank?f=rate1&o=0&s=30',headers=headers).text
+	data.update({'item_data': res, 'is_down': 0})
+	con.execute(sql, data)
+	
+	
+
+
 if __name__ == '__main__':
 # 	try:
 # 		proc()
 # 	except:
-	proc_json()
-	df=df.drop_duplicates()
-	df.to_csv('bili_fans/down_fans.csv',header=True,index=True)
+
+	main()
+	# proc_json()
+	# df=df.drop_duplicates()
+	# df.to_csv('bili_fans/down_fans.csv',header=True,index=True)
